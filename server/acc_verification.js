15b5856bad9821787910cf5196d414f9b14c7bd4
endragor <endragorr@gmail.com>
2013-05-18 19:41:59 +0700

    legnth limit for regform

--- server/acc_verification.js
+++ server/acc_verification.js
@@ -3,7 +3,7 @@ Accounts.validateNewUser(function(user) {
 		throw new Meteor.Error(403, 'Profile required');
 	}
 
-	var usernameRe = /^[a-z 0-9_]+$/i;
+	var usernameRe = /^[a-z 0-9_]{1,40}$/i;
 	var username = user.username;
 	if (!username || typeof(username) !== 'string' || !usernameRe.test(username) || username === 'h34dump') {
 		throw new Meteor.Error(403, 'Bad username');
@@ -15,11 +15,11 @@ Accounts.validateNewUser(function(user) {
 
 	var email = user.emails[0].address;
 	var emailRe = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	if (!emailRe.test(email)) {
+	if (!emailRe.test(email) || email.length > 100) {
 		throw new Meteor.Error(403, 'Bad email');
 	}
 
-	var countryRe = /^[a-z ]*$/i;
+	var countryRe = /^[a-z ]{0,40}$/i;
 	var country = user.profile.country;
 	if (country && (typeof(country) !== 'string' || !countryRe.test(country))) {
 		throw new Meteor.Error(403, 'Bad country');
